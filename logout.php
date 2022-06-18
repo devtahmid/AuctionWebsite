58574085168c264dc745e2bd3e4399efb616b59a
devtahmid <shaikhtahmidurrahman1@gmail.com>
2022-06-18 12:22:01 +0300

    logout fixed. userId session destroyed

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
 <?php
 unset($_SESSION['user_id']);
 unset($_SESSION['auction_id']);
+session_unset();
 session_destroy();
+//below from php website session unset function
+session_write_close();
+setcookie(session_name(),'',0,'/');
 header("location:login_form.php");
 ?>
